1c6f319d0b9ccd9a9699e3751a058532aada82fd
sally375 <sally_donovan@worksafe.vic.gov.au>
2025-11-25 09:47:29 +1100

    Update streamlit_app.py
    ensuring correct value is searched on.

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,12 +15,9 @@ st.write('The name on your smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'), col('search_on'))
-#st.dataframe(data = my_dataframe, use_container_width = True)
-#st.stop()
+
 # Convert the Snowpark dataframe to a pandas dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
-#st.dataframe(pd_df)
-#st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
@@ -38,7 +35,7 @@ if ingredients_list:
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
    
     my_insert_stmt = """ insert into smoothies.public.orders(name_on_order, ingredients)
